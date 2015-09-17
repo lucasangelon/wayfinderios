991c925445db7cf8b3685b85ec05ecc746a389af
SUCHI <bapat.suruchi@gmail.com>
2015-09-17 11:46:56 +0800

    Base Code for Wayfinder
    Base Code for WayFinder

---
+++ GMapsDemo/ListCampusViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ListCampusViewController.swift
+//  CentralWayFinder
+//
+//  Created by Guanghui Ji on 15/09/2015.
+//  Copyright © 2015 Appcoda. All rights reserved.
+//
+
+import UIKit
+
+class ListCampusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
